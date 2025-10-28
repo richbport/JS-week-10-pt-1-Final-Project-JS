4b5397028ee44c135f97eb4bcb6cbd9a309d1a1d
Rich <richbport@yahoo.com>
2025-10-28 15:25:35 -0400

    renderMovies

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ function searchChange(event) {
     console.log(event.target.value)
 }
 
-console.log(fetch('https://omdbapi.com/?s=batman%apikey=4c2eefb7'))
+function renderMovies() {
+    console.log(fetch('https://omdbapi.com/?s=batman%apikey=4c2eefb7'))
+}
+
+renderMovies()

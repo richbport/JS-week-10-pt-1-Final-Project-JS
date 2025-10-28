ebe036740d8530db33b029b14f774beb66bd41af
Rich <richbport@yahoo.com>
2025-10-28 15:43:10 -0400

    api link correction

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function searchChange(event) {
     console.log(event.target.value)
 }
 
-function renderMovies() {
-    console.log(fetch('https://omdbapi.com/?s=batman%apikey=4c2eefb7'))
+async function renderMovies() {
+    console.log( await fetch('http://omdbapi.com/?s=batman&apikey=4c2eefb7'))
 }
 
 renderMovies()

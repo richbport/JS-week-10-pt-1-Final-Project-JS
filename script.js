6a9200d7713673ab01b335a9d1735a5b9b8d8b79
Rich <richbport@yahoo.com>
2025-10-31 16:32:36 -0400

    movieList

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-const moviesWrapper = document.querySelector('.movies')
-const searchName = document.querySelector('.searchName')
+const moviesWrapper = document.querySelector(".movies");
+const searchName = document.querySelector(".searchName");
 
 function searchChange(event) {
-    renderMovies(event.target.value)
-    searchName.innerHTML = event.target.value
+  renderMovies(event.target.value);
+  searchName.innerHTML = event.target.value;
 }
 
 async function renderMovies(searchTerm) {
-  console.log("This is the search term", searchTerm)
-    const response = await fetch(`http://omdbapi.com/?s=${searchTerm}&apikey=4c2eefb7`)
-    const data = await response.json()
-    const moviesArr = data.Search
-    console.log(moviesArr)
-    moviesWrapper.innerHTML = moviesArr.slice(0, 6).map((movie) => {
-        return `
+  console.log("This is the search term", searchTerm);
+  const response = await fetch(
+    `http://omdbapi.com/?s=${searchTerm}&apikey=4c2eefb7`
+  );
+  const data = await response.json();
+  const moviesArr = data.Search;
+  console.log(moviesArr);
+  displayMovies(moviesArr);
+}
+
+function displayMovies(movieList) {
+  moviesWrapper.innerHTML = movieList
+    .slice(0, 6)
+    .map((movie) => {
+      return `
         <div class="movie">
         <img src=${movie.Poster} alt="" />
         <h2>${movie.Title}</h2>
         <h4>${movie.Year}</h4>
         <button>Learn More</button>
         </div>
-        `
-    }).join("")
+        `;
+    })
+    .join("");
 }
